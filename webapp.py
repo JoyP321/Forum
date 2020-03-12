8733753c381ea36ae768dec625b5315daee37baa
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2020-03-12 10:10:15 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -76,17 +76,19 @@ def render_thread():
     print(request.args('threadName'))
     return render_template('thread.html',threadName = request.args('threadName'), posts= format_docs(request.args('threadName'))
 
-def format_docs(myCollection): 
-    toReturn = ''
-    for doc in myCollection.find():
-        toReturn += "/n" + doc
-    return toReturn
 
 def get_threads():
     toReturn = ''
     myList = db.list_collection_names()
     for collection in myList:
         toReturn += Markup("<input type='radio' onclick='myFunction()' name = 'threadName' value='" + collection + "'>" + collection + "<br>")
+    return toReturn
+                          
+
+def format_docs(myCollection): 
+    toReturn = ''
+    for doc in myCollection.find():
+        toReturn += "/n" + doc
     return toReturn
 
 @github.tokengetter #runs automatically. needed to confirm logged in
