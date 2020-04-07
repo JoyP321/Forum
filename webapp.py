a9164d778d747d780d5aeca626e325ad12b0fc4d
JoyP321 <54326509+JoyP321@users.noreply.github.com>
2020-04-07 10:17:32 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -42,7 +42,7 @@ def home():
 
 @app.route('/threadAdded', methods=['GET','POST'])
 def threadAdded():
-    db.createCollection(request.form['newThread'])
+    db.createCollection('hellllo')#request.form['newThread'])
     return render_template('home.html', threads = get_threads())
     
 @app.route('/login')
